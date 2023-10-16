1c7c1f38b1e15c9ff0438a8837f7e7b0b53f8ae4
nguyen8amk1 <trungnguyen123akohshit@gmail.com>
2023-10-16 21:08:49 +0700

    add something new

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,9 @@
 
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
-    application
+    application 
+    java 
+    eclipse
 }
 
 repositories {
