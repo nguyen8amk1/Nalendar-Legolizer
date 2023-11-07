d42a8d7088d5cf34cde1a9224af14e7514dd10a0
nguyen8amk1 <trungnguyen123akohshit@gmail.com>
2023-11-07 14:13:42 +0700

    Create RayTracingTest.java

---
+++ core_module/src/main/java/nttn/legolization/core/RayTracingTest.java
@@ -0,0 +1,15 @@
+package nttn.legolization.core;
+
+public class RayTracingTest {
+    /*
+    TODO: write a ray tracing to 3d triangle mesh
+            output ppm image
+    * */
+    void raytraceToPpm(String filename) {
+        // TODO: ray trace a sphere first
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
