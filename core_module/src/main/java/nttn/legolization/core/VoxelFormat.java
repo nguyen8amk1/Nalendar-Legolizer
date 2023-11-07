95c563ff4f78693bad9167cccd5998b3f3b8617b
nguyen8amk1 <trungnguyen123akohshit@gmail.com>
2023-11-07 08:28:47 +0700

    nothing great though

---
+++ core_module/src/main/java/nttn/legolization/core/VoxelFormat.java
@@ -0,0 +1,4 @@
+package nttn.legolization.core;
+
+public class VoxelFormat {
+}
--- core_module/src/main/java/nttn/legolization/core/Voxelizer.java
+++ core_module/src/main/java/nttn/legolization/core/Voxelizer.java
@@ -8,15 +8,17 @@ public class Voxelizer {
         output: 3d voxelize information (a voxel format that can be rendered using our 3d engine)
             -> a list of voxel position ?? -> this seems BETTER
             -> or just a class that comes with a render method ? (this will be hard to batch)
-
+         -> We gonna do "Voxel ray casting"
         // for now we just use void as return type
     * */
 
-    public static void voxelize(String filename) {
+    public static VoxelFormat voxelize(String filename) {
         if(!FileFormatChecker.fileFormatIsSupported(filename)) {
             System.out.println("File format not supported");
-            return;
+            return null;
         }
+        VoxelFormat voxel = new VoxelFormat();
         System.out.println("About to voxelize the 3d file " + filename);
+        return voxel;
     }
 }
